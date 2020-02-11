fc907316a622562ad5eb6fe574555c06cc56e43b
asmi-kafle <kafleasmi1@gmail.com>
2020-02-11 03:30:19 -0600

    Initial Commit

---
+++ shell.h
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h> 
+#include <fcntl.h>
+#include <vector>
+#include <string>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pwd.h>
+#include <limits.h>
+#include <iostream>
+
+using namespace std;
+
+//to get shell command
+
+//to execute commands
+
+//to get User
+
+//to get Host
+
+//to handle tokens
+
+//pipes
+
+//parsers
+
